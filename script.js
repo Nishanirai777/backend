d808be1acaf0d43bd61704a430265bb1a1d27d75
Nisha <rainishani400@gmail.com>
2024-11-19 21:22:13 +0545

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ let arg=  process.argv;
 for(let i=0; i<arg.length; i++){
     console.log("hello & welcome to " , arg[i]);
 }
+//let n=9;
+for(let i =0; i<n; i++){
+    console.log("hello" + i);
+    
+}
+console.log("bye")
