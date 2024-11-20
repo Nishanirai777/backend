e8168178bc086b9a7fa46d54d06b5a3a943e8a41
Nisha <rainishani400@gmail.com>
2024-11-20 17:12:37 +0545

    Add files via upload

---
+++ backend(day11)/script.js
@@ -0,0 +1,19 @@
+//let arg=  process.argv;
+//for(let i=0; i<arg.length; i++){
+   // console.log("hello & welcome to " , arg[i]);
+//}
+//const info = require("fruits");
+//console.log(info);
+//const figlet = require("figlet");
+//figlet("i love you!!", function (err, data) {
+   //if (err) {
+     //console.log("Something went wrong...");
+     //console.dir(err);
+     //return;
+   //}
+   //console.log(data);
+ //});
+ import { sum ,PI} from "./math.js";
+ import { generate } from "random-words";
+ //console.log(sum(1,5));
+ console.log(generate());
